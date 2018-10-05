8d7b118604da29760e5a5afd4b5cebd72ecb8500
Julia Naomi <jnaomi@thoughtworks.com>
2018-10-05 00:51:24 -0300

    Makes Hyper serve a Magic Cards Json

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My Rust Graphql Api based on Magic The Gathering cards and possibly other game c
 
 ## Roadmap
 [x] - Create Rust Server
+[x] - Makes Hyper server serve Magic Cards Json
 [] - Create GraphQL Query
 [] - DEVOPS
 [] - Consume JSON Data
@@ -19,3 +20,4 @@ My Rust Graphql Api based on Magic The Gathering cards and possibly other game c
   * TravisCI
 4. Server dependencies:
   * Hyper
+  * Serde
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,80 @@
+#[macro_use] extern crate serde_derive;
 extern crate hyper;
+#[macro_use] extern crate serde_json;
+extern crate serde;
+extern crate futures;
 
-use hyper::{Body, Response, Server};
+use std::fs::File;
+use std::io::prelude::*;
+
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::{Request, Body, Response, Server};
+use hyper::service::{service_fn_ok};
 
-static TEXT: &str = "pong";
 
 fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let ping_svc = || {
-        service_fn_ok(|_req|{
-            Response::new(Body::from(TEXT))
-        })
+    let ping_svc = move || {
+        service_fn_ok(fake_json)
     };
+
+
 
     let server = Server::bind(&addr)
         .serve(ping_svc)
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
+}
+
+fn read_fake() -> std::io::Result<String> {
+    let mut file = File::open("./body.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn fake_json(_req: Request<Body>) -> Response<Body> {
+    let v: Card = serde_json::from_str(read_fake().unwrap().as_str()).unwrap();
+
+    Response::new(Body::from(json!(v.clone()).to_string()))
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Card {
+    cards: Vec<CardBody>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct CardBody {
+    name: String,
+    mana_cost: Option<String>,
+    cmc: i32,
+    colors: Vec<String>,
+    color_identity: Option<Vec<String>>,
+    types: Vec<String>,
+    subtypes: Option<Vec<String>>,
+    rarity: String,
+    set: String,
+    set_name: Option<String>,
+    text: String,
+    artist: String,
+    number: String,
+    power: Option<String>,
+    toughness: Option<String>,
+    layout: String,
+    multiverseid: i32,
+    image_url: Option<String>,
+    rulings: Option<Vec<Rulings>>,
+    printings: Vec<String>,
+    original_text: Option<String>,
+    original_type: Option<String>,
+    id: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Rulings {
+    date: String,
+    text: String,
 }
